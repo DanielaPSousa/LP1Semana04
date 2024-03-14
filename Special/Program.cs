e3d43524b96130ecaa78ee2b0be7fe4d50d31da5
Daniela Sousa <danielasousa.info@gmail.com>
2024-03-14 12:08:49 +0000

    Fixed some lines of the code

--- Special/Program.cs
+++ Special/Program.cs
@@ -6,10 +6,10 @@ namespace Special
     {
         private static int GetSpecial(int n)
         {
-            if (n == 0);
+            if (n == 0)
                 return 0;
             
-            if (n == 1);
+            if (n == 1)
                 return 1;
             GetSpecial (n - 1) + 2*GetSpecial(n - 2);
         }
@@ -17,10 +17,10 @@ namespace Special
         static void Main(string[], args)
         {
             Console.WriteLine("Insert n-th number of the special sequence:");
-            int n= int.Parse(Console.WriteLine());
+            int n= int.Parse(Console.ReadLine());
 
             int sp= GetSpecial(n);
-            Console.WriteLine= (sp);
+            Console.WriteLine(sp);
         }
     }
 }
